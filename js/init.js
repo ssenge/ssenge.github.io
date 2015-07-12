6ad8750732a2ee549cfafbf684b42bf2ea9a98d5
sese <sese@foobar1981.com>
2015-07-12 10:21:25 +0200

    GA, JS, ...

--- js/init.js
+++ js/init.js
@@ -1,9 +1,3 @@
-/*
-	Verti by HTML5 UP
-	html5up.net | @n33co
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
-
 (function($) {
 
 	skel.init({
@@ -73,11 +67,11 @@
 			}
 
 		// Dropdowns.
-			$('#nav > ul').dropotron({
-				mode: 'fade',
-				noOpenerFade: true,
-				speed: 300
-			});
+		//	$('#nav > ul').dropotron({
+		//		mode: 'fade',
+		//		noOpenerFade: true,
+		//		speed: 300
+		//	});
 
 	});
 
